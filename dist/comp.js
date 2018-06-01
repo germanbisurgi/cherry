a7356372471f9a7461b89af9bd376249123598b6
German Bisurgi <gebisurgi@gmail.com>
2018-06-01 23:06:47 +0200

    dist

--- dist/comp.js
+++ dist/comp.js
@@ -1,24 +1,12 @@
-<<<<<<< Updated upstream
 var comp = {};
 
 comp.objectPool = function (config) {
   'use strict';
   this.config = config || {};
-=======
-var ObjectPool = function ObjectPool (config) {
-  'use strict';
-  this.config = config || {};
   this.pool = [];
   this.used = 0;
 };
 
-ObjectPool.prototype.clear = function () {
->>>>>>> Stashed changes
-  this.pool = [];
-  this.used = 0;
-};
-
-<<<<<<< Updated upstream
 comp.objectPool.prototype.clear = function () {
   this.pool = [];
   this.used = 0;
@@ -76,76 +64,12 @@ comp.objectPool.prototype.each = function (fn) {
 };
 
 comp.gameLoop = function (config) {
-=======
-ObjectPool.prototype.use = function () {
-
-  // get a free object
-  var unusedItem = false;
-  this.pool.forEach(function (item) {
-    if (item.active === false) {
-      unusedItem = item;
-    }
-  });
-
-  // if free object init and reuse it
-  if (unusedItem) {
-    this.config.reset.apply(this, [unusedItem.object].concat(Array.prototype.slice.call(arguments)));
-    unusedItem.active = true;
-    return unusedItem.object;
-  }
-
-  // if no free object creates one
-  var item = {
-    active: true,
-    object: new (Function.prototype.bind.apply(this.config.class, [null].concat(Array.prototype.slice.call(arguments))))()
-  };
-  this.pool.push(item);
-  this.used++;
-  return item.object;
-};
-
-ObjectPool.prototype.dismiss = function (obj) {
-  // search o and deactivate it
-  this.pool.forEach(function (item) {
-    if (item.object === obj) {
-      item.active = false;
-    }
-  });
-  this.used--;
-};
-
-ObjectPool.prototype.size = function () {
-  return this.pool.length;
-};
-
-ObjectPool.prototype.each = function (fn) {
-  var length = this.pool.length;
-  var i;
-  for (i = 0; i < length; i++) {
-    if (this.pool[i].active === true) {
-      fn(this.pool[i].object, i);
-    }
-  }
-};
-
-if (typeof module !== 'undefined') {
-  module.exports = ObjectPool;
-}
-
-var ObjectPool = require('./object-pool');
-
-var GameLoop = function (config) {
->>>>>>> Stashed changes
   'use strict';
   config = config || {};
   this.delta = 0;
   this.fps = config.fps || 60;
   this.frame = 0;
-<<<<<<< Updated upstream
   this.tasks = new comp.objectPool({
-=======
-  this.tasks = new ObjectPool({
->>>>>>> Stashed changes
     class: function (delay, task) {
       this.delay = delay;
       this.task = task;
@@ -157,11 +81,7 @@ var GameLoop = function (config) {
   });
 };
 
-<<<<<<< Updated upstream
 comp.gameLoop.prototype.tick = function () {
-=======
-GameLoop.prototype.tick = function () {
->>>>>>> Stashed changes
   this.frame++;
   var self = this;
   this.tasks.each(function (task) {
@@ -174,18 +94,10 @@ GameLoop.prototype.tick = function () {
   });
 };
 
-<<<<<<< Updated upstream
 comp.gameLoop.prototype.queueTask = function (delay, task) {
-=======
-GameLoop.prototype.queueTask = function (delay, task) {
->>>>>>> Stashed changes
   this.tasks.use(delay, task);
 };
 
 if (typeof module !== 'undefined') {
-<<<<<<< Updated upstream
   module.exports = comp;
-=======
-  module.exports = GameLoop;
->>>>>>> Stashed changes
 }
