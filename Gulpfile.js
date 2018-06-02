a0a6c7344fe405e1f5dd445311650a376239d477
gerhus <gebisurgi@gmail.com>
2018-06-02 17:46:38 +0200

    dist-concat and test-concat

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,17 +1,28 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var scripts = [
+  './src/intro.js',
+  './src/polyfills.js',
+  './src/pool.js',
+  './src/loop.js',
+  './src/signal.js',
+  './src/game.js',
+  './src/state.js',
+  './src/outro.js'
+];
 
-gulp.task('concat', function () {
-  gulp.src([
-    './src/intro.js',
-    './src/object-pool.js',
-    './src/game-loop.js',
-    './src/outro.js'
-  ])
+gulp.task('concat-dist', function () {
+  gulp.src(scripts)
     .pipe(concat('comp.js'))
     .pipe(gulp.dest('./dist'));
 });
 
+gulp.task('concat-test', function () {
+  gulp.src(scripts)
+    .pipe(concat('comp.js'))
+    .pipe(gulp.dest('./tests/manual'));
+});
+
 gulp.task('watch', function () {
-  gulp.watch('./src/*.js', ['concat']);
+  gulp.watch('./src/*.js', ['concat-dist', 'concat-test']);
 });
