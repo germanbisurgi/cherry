44ee36fbfee072bec477ecff51c629d10561116e
German Bisurgi <gebisurgi@gmail.com>
2018-06-03 18:47:43 +0200

    do not call state methods if there is no state

--- src/game.js
+++ src/game.js
@@ -4,19 +4,22 @@ comp.game = function (config) {
   self.states = new comp.stateManager(self);
 
   self.loop.update = function () {
+    if (self.states.current !== null) {
 
-    if (!self.states.current.preloaded) {
-      self.states.current.preloaded = true;
-      self.states.current.preload(self);
-    }
+      if (!self.states.current.preloaded) {
+        self.states.current.preloaded = true;
+        self.states.current.preload(self);
+      }
 
-    if (!self.states.current.created) {
-      self.states.current.created = true;
-      self.states.current.create(self);
-    }
+      if (!self.states.current.created) {
+        self.states.current.created = true;
+        self.states.current.create(self);
+      }
 
-    if (self.states.current.created) {
-      self.states.current.update(self);
+      if (self.states.current.created) {
+        self.states.current.update(self);
+      }
+
     }
   };
 
