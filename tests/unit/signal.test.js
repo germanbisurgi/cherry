37a64a3b66041f38936c833251129318f6e02368
German Bisurgi <gebisurgi@gmail.com>
2018-06-05 21:40:09 +0200

    linted code

--- tests/unit/signal.test.js
+++ tests/unit/signal.test.js
@@ -9,36 +9,36 @@ describe('Signal', function () {
     expect(signal.getTest()).toBe(null);
   });
   // it('should add listener', function () {
-    // expect(signal.getTest()).toBe(null);
+  // expect(signal.getTest()).toBe(null);
   // });
   // it('should dispatch signal passing custom parameters', function () {
-    // expect(signal.getTest()).toBe(null);
+  // expect(signal.getTest()).toBe(null);
   // });
   // it('should remove a single listener', function () {
-    // expect(signal.getTest()).toBe(null);
+  // expect(signal.getTest()).toBe(null);
   // });
   // it('should remove all listeners', function () {
-    // expect(signal.getTest()).toBe(null);
+  // expect(signal.getTest()).toBe(null);
   // });
   // it('should add one time listener', function () {
-    // expect(signal.getTest()).toBe(null);
+  // expect(signal.getTest()).toBe(null);
   // });
   // it('should disable', function () {
-    // expect(signal.getTest()).toBe(null);
+  // expect(signal.getTest()).toBe(null);
   // });
   // it('should enable', function () {
-    // expect(signal.getTest()).toBe(null);
+  // expect(signal.getTest()).toBe(null);
   // });
   // it('should prevent next listeners on the queue from being executed', function () {
-    // expect(signal.getTest()).toBe(null);
+  // expect(signal.getTest()).toBe(null);
   // });
   // it('should execute by priority', function () {
-    // expect(signal.getTest()).toBe(null);
+  // expect(signal.getTest()).toBe(null);
   // });
   // it('should disable signal binding', function () {
-    // expect(signal.getTest()).toBe(null);
+  // expect(signal.getTest()).toBe(null);
   // });
   // it('should enable signal binding', function () {
-    // expect(signal.getTest()).toBe(null);
+  // expect(signal.getTest()).toBe(null);
   // });
 });
