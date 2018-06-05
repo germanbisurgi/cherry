18e6ec6c077e981880264265ddc47273288467ea
German Bisurgi <gebisurgi@gmail.com>
2018-06-05 19:23:46 +0200

    updated test

--- tests/unit/loop.test.js
+++ tests/unit/loop.test.js
@@ -9,7 +9,7 @@ describe('Loop', function () {
     expect(loop.getFps()).toBe(60);
     expect(loop.getFrame()).toBe(0);
     expect(loop.getStatus()).toBe('off');
-    expect(loop.getTasks().size()).toBe(0);
+    expect(loop.queuedTasks.size()).toBe(0);
     expect(loop.getTimestep()).toBe(1000 / 60);
   });
   it('should functional public setters', function () {
@@ -25,7 +25,7 @@ describe('Loop', function () {
     expect(loop.getFps()).toBe(60);
     expect(loop.getFrame()).toBe(0);
     expect(loop.getStatus()).toBe('off');
-    expect(loop.getTasks().size()).toBe(0);
+    expect(loop.queuedTasks.size()).toBe(0);
   });
   it('should increment the frame number and update last time on step() ', function () {
     loop.step();
