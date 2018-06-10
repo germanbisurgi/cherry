944f149827df468cc925d77907b8b913a53f73fd
gerhus <gebisurgi@gmail.com>
2018-06-10 16:42:11 +0200

    refactored code

--- tests/manual/game.js
+++ tests/manual/game.js
@@ -1,27 +1,24 @@
 window.addEventListener('load', function () {
+
   var canvas = document.querySelector('canvas');
   var context = canvas.getContext('2d');
+  var game = new cherry.Game();
+  var state = new cherry.State('test');
   var debug = new cherry.Debug(context);
-  var testState = new cherry.State('test');
 
-  testState.preload = function (game) {};
-
-  testState.create = function (game) {};
-
-  testState.update = function (game) {
-
+  state.preload = function (game) {};
+  state.create = function (game) {};
+  state.update = function (game) {
     context.clearRect(0, 0, canvas.width, canvas.height);
-
     debug.print(10, 10, {
       frame: 'frame: ' + game.loop.frame,
       delta: 'delta: ' + game.loop.delta,
       fps: 'fps: ' + 1 / game.loop.delta * 1000
     });
-
   };
 
-  var game = new cherry.Game();
-  game.states.add(testState);
+  game.states.add(state);
   game.states.switch('test');
   game.loop.start();
+
 });
