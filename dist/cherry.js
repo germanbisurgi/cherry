f07bca0f4e5a8664c9f1772562812519b5905ddb
gerhus <gebisurgi@gmail.com>
2018-06-10 16:33:27 +0200

    added debug class plus test

--- dist/cherry.js
+++ dist/cherry.js
@@ -13,6 +13,24 @@ window.requestAnimFrame = function () {
   );
 }();
 
+cherry.Debug = function (context) {
+  this.context = context;
+  this.fontSize = 15;
+  this.line = 1;
+};
+
+cherry.Debug.prototype.print = function (x, y, lines) {
+  this.context.save();
+  this.context.font = this.fontSize + 'px monospace';
+  this.context.textAlign = 'start';
+  for (var prop in lines) {
+    this.context.fillText(lines[prop], x, y + (this.line * this.fontSize));
+    this.line++;
+  }
+  this.context.restore();
+  this.line = 1;
+};
+
 cherry.Game = function () {
   this.loop = new cherry.Loop();
   this.states = new cherry.StateManager(this);
