05ce428f9d1fb906bb738fc424d9a07290b23d3d
German Bisurgi <gebisurgi@gmail.com>
2018-12-07 20:28:11 +0100

    updated loop. Added scripts

--- docs/scripts/naive.js
+++ docs/scripts/naive.js
@@ -11282,33 +11282,17 @@ var Loop = function () {
   this.lastStep = 0;
   this.fps = 60;
   this.frame = 0;
-  this.status = 'off';
+  this.paused = true;
   this.timestep = 1000 / this.fps;
-
-  var queuedTask = function (fn) {
-    this.execute = fn;
-  };
-
-  this.queuedTasks = new naive.Pool(queuedTask, function (object, fn) {
-    object.execute = fn;
-  });
-
-};
-
-Loop.prototype.executeQueuedTasks = function () {
-  this.queuedTasks.each(function (task) {
-    task.execute();
-    this.queuedTasks.dismiss(task);
-  }.bind(this));
-};
-
-Loop.prototype.nextStep = function (task) {
-  this.queuedTasks.use(task);
 };
 
 Loop.prototype.start = function () {
-  this.status = 'on';
+  this.paused = false;
   window.requestAnimationFrame(this.run.bind(this));
+};
+
+Loop.prototype.stop = function () {
+  this.paused = true;
 };
 
 Loop.prototype.run = function (timestamp) {
@@ -11321,14 +11305,13 @@ Loop.prototype.run = function (timestamp) {
     this.lastStep = timestamp;
     this.accumulator -= this.timestep;
   }
-  if (this.status === 'on') {
+  if (!this.paused) {
     window.requestAnimationFrame(this.run.bind(this));
   }
 };
 
 Loop.prototype.step = function () {
   this.frame++;
-  this.executeQueuedTasks();
   this.onStep();
 };
 
--- tests/unit/loop.test.js
+++ tests/unit/loop.test.js
@@ -11,21 +11,16 @@ describe('Loop', function () {
     expect(loop.delta).toBe(0);
     expect(loop.fps).toBe(60);
     expect(loop.frame).toBe(0);
-    expect(loop.status).toBe('off');
+    expect(loop.paused).toBe(true);
     expect(loop.timestep).toBe(1000 / 60);
-    expect(loop.queuedTasks.size).toBe(0);
   });
   it('should increment the frame number and update last time on step() and execute the onStep() function', function () {
     loop.step();
     expect(loop.frame).toBe(1);
     expect(stepped).toBe(true);
   });
-  it('should execute a queued task in the next step', function () {
-    loop.nextStep(function () {
-      taskExecuted = true;
-    });
-    expect(taskExecuted).toBe(false);
-    loop.step();
-    expect(taskExecuted).toBe(true);
+  it('should stop execution', function () {
+    loop.stop();
+    expect(loop.paused).toBe(true);
   });
 });
